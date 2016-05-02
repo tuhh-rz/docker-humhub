642c3e69e8926bf1a60d35a254308cf0e2ce63c5
root <root@r6a-srv-042.zll.tu-harburg.de>
2016-05-02 15:54:32 +0200

    Initial

---
+++ index.php
@@ -0,0 +1,131 @@
+    <!DOCTYPE html>
+    <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+    <head>
+        <title></title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+        <meta name="language" content="en"/>
+        <meta name="csrf-param" content="_csrf">
+    <meta name="csrf-token" content="ZFlqcnBlYkwpH0cEGywbNAAwRy0YICc5VxwtPRIdVQIrIBAHOCoGew==">
+
+        <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css"/>
+        <link type="text/css" href="/css/animate.min.css" rel="stylesheet"/>
+        <link href="/assets/65dc8f0a/css/bootstrap.css?v=1461763200" rel="stylesheet">
+<link href="/assets/85f948e5/css/font-awesome.min.css?v=1461763201" rel="stylesheet">
+<link href="/css/animate.min.css?v=1461762945" rel="stylesheet">
+<link href="/css/temp.css?v=1461762945" rel="stylesheet">
+<link href="/css/bootstrap-wysihtml5.css?v=1461762945" rel="stylesheet">
+<link href="/css/flatelements.css?v=1461762945" rel="stylesheet">
+<link href="/resources/at/jquery.atwho.css?v=1461762949" rel="stylesheet">
+        <!-- start: render additional head (css and js files) -->
+        <link href="/themes/HumHub/css/theme.css" rel="stylesheet">
+<link href="/themes/HumHub/font/open_sans/open-sans.css" rel="stylesheet">
+        <!-- end: render additional head -->
+    </head>
+    <body>
+    <script src="/assets/e26d8472/jquery.js?v=1461763200"></script>
+<script src="/assets/1c3c24c5/yii.js?v=1461763200"></script>
+<script src="/assets/65dc8f0a/js/bootstrap.js?v=1461763200"></script>
+<script src="/assets/cd62ae8a/jquery.timeago.js?v=1461763200"></script>
+<script src="/assets/bdedc802/dist/jquery.knob.min.js?v=1461763200"></script>
+<script src="/assets/99bc0f63/ui/minified/widget.min.js?v=1461763201"></script>
+<script src="/assets/90f3617d/jquery.placeholder.min.js?v=1461763201"></script>
+<script src="/assets/9bff91bc/js/jquery.fileupload.js?v=1461763201"></script>
+<script src="/assets/9bff91bc/js/jquery.iframe-transport.js?v=1461763201"></script>
+<script src="/js/ekko-lightbox-modified.js?v=1461762945"></script>
+<script src="/js/modernizr.js?v=1461762945"></script>
+<script src="/js/jquery.cookie.js?v=1461762945"></script>
+<script src="/js/jquery.highlight.min.js?v=1461762945"></script>
+<script src="/js/jquery.autosize.min.js?v=1461762945"></script>
+<script src="/js/wysihtml5-0.3.0.js?v=1461762945"></script>
+<script src="/js/bootstrap3-wysihtml5.js?v=1461762945"></script>
+<script src="/js/jquery.color-2.1.0.min.js?v=1461762945"></script>
+<script src="/js/jquery.flatelements.js?v=1461762945"></script>
+<script src="/js/jquery.loader.js?v=1461762945"></script>
+<script src="/js/desktop-notify-min.js?v=1461762945"></script>
+<script src="/js/desktop-notify-config.js?v=1461762945"></script>
+<script src="/resources/at/jquery.caret.min.js?v=1461762949"></script>
+<script src="/resources/at/jquery.atwho.min.js?v=1461762949"></script>
+<script src="/resources/file/fileuploader.js?v=1461762949"></script>
+<script src="/resources/user/userpicker.js?v=1461762949"></script>
+<script src="/js/jquery.nicescroll.min.js?v=1461762945"></script>
+<script src="/js/app.js?v=1461762945"></script>
+    <div class="container installer" style="margin: 0 auto; max-width: 700px;">
+        <div class="logo">
+            <a class="animated fadeIn" href="http://www.humhub.org" target="_blank" class="">
+                <img src="/resources/installer/humhub-logo.png" alt="Logo">
+            </a>
+        </div>
+
+        
+<div class="panel panel-default animated fadeIn">
+
+    <div class="panel-body text-center">
+        <br>
+        <br>
+        <p class="lead"><strong>Welcome</strong> to HumHub<br>Your Social Network Toolbox</p>
+        <p>This wizard will install and configure your own HumHub instance.<br><br>To continue, click Next.</p>
+        <br>
+        <hr>
+        <br>
+        <a class="btn btn-lg btn-primary" href="/index.php?r=installer%2Findex%2Fgo">Next <i class="fa fa-arrow-circle-right"></i></a>        <br>
+        <br>
+    </div>
+
+
+</div>
+
+<div class="text text-center animated fadeIn">
+    Choose language: &nbsp;
+    <div class="langSwitcher">
+        <form id="choose-language-form" action="/index.php?r=installer%2Findex" method="post">
+<input type="hidden" name="_csrf" value="ZFlqcnBlYkwpH0cEGywbNAAwRy0YICc5VxwtPRIdVQIrIBAHOCoGew==">        <select id="chooselanguage-language" name="ChooseLanguage[language]" onChange="this.form.submit()">
+<option value="en">English (US)</option>
+<option value="en_gb">English (UK)</option>
+<option value="de">Deutsch</option>
+<option value="fr">Français</option>
+<option value="nl">Nederlands</option>
+<option value="pl">Polski</option>
+<option value="pt">Português</option>
+<option value="pt_br">Português do Brasil</option>
+<option value="es">Español</option>
+<option value="ca">Català</option>
+<option value="it">Italiano</option>
+<option value="th">ไทย</option>
+<option value="tr">Türkçe</option>
+<option value="ru">Русский</option>
+<option value="uk">українська</option>
+<option value="el">Ελληνικά</option>
+<option value="ja">日本語</option>
+<option value="hu">Magyar</option>
+<option value="nb_no">Nnorsk bokmål</option>
+<option value="zh_cn">中文(简体)</option>
+<option value="zh_tw">中文(台灣)</option>
+<option value="an">Aragonés</option>
+<option value="vi">Tiếng Việt</option>
+<option value="sv">Svenska</option>
+<option value="cs">čeština</option>
+<option value="da">dansk</option>
+<option value="uz">Ўзбек</option>
+<option value="fa_ir">فارسی</option>
+<option value="bg">български</option>
+<option value="sk">slovenčina</option>
+<option value="ro">română</option>
+<option value="ar">العربية/عربي‎‎</option>
+<option value="ko">한국어</option>
+<option value="id">Bahasa Indonesia</option>
+<option value="lt">lietuvių kalba</option>
+</select>        </form>    </div>
+</div>
+        <div class="text text-center powered">
+            Powered by <a href="http://www.humhub.org" target="_blank">HumHub</a>
+            <br/>
+            <br/>
+        </div>
+    </div>
+
+    <div class="clear"></div>
+    <script src="/assets/1c3c24c5/yii.activeForm.js?v=1461763200"></script>
+<script type="text/javascript">jQuery(document).ready(function () {
+jQuery('#choose-language-form').yiiActiveForm([], []);
+});</script>    </body>
+    </html>
