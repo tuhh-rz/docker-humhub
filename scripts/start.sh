9e97ba27417d0ff3ccef472059b33d4a81cd84a2
Andreas Böttger <andreas.boettger@tuhh.de>
2017-07-04 11:41:08 +0000

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -61,7 +61,7 @@ fi
 
 # Tweak nginx to match the workers to cpu's
 
-procs=$(cat /proc/cpuinfo |grep processor | wc -l)
+procs=$(grep -c processor /proc/cpuinfo)
 sed -i -e "s/worker_processes 5/worker_processes $procs/" /etc/nginx/nginx.conf
 
 # Very dirty hack to replace variables in code with ENVIRONMENT values
