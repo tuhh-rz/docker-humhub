bd7f3e9f6b72b4fbf0c1598f9e127c8985b14b29
Andreas Böttger <andreas.boettger@tuhh.de>
2018-08-06 09:22:42 +0200

    add version 1.3

---
+++ 1.3/scripts/start.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+rsync -rc /tmp/humhub/* /usr/share/nginx/html
+
+# Display PHP error's or not
+if [[ "$ERRORS" != "1" ]] ; then
+  sed -i -e "s/error_reporting =.*=/error_reporting = E_ALL/g" /etc/php/7.0/fpm/php.ini
+  sed -i -e "s/display_errors =.*/display_errors = On/g" /etc/php/7.0/fpm/php.ini
+fi
+
+# Tweak nginx to match the workers to cpu's
+
+procs=$(cat /proc/cpuinfo |grep processor | wc -l)
+sed -i -e "s/worker_processes 5/worker_processes $procs/" /etc/nginx/nginx.conf
+
+
+mkdir -p /run/php/
+
+rm -rf /usr/share/nginx/html/protected/runtime/cache
+
+su -s /bin/sh -c 'yes | php /usr/share/nginx/html/protected/yii migrate/up --includeModuleMigrations=1' www-data
+chown -R www-data:www-data /usr/share/nginx/html/
+
+# Start supervisord and services
+exec /usr/bin/supervisord -n -c /etc/supervisord.conf
