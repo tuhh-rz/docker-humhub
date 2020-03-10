97e7c3bbbfc1d6f349063d826757734abdd834a4
Andreas Böttger <andreas.boettger@tuhh.de>
2020-03-10 13:34:04 +0100

    use php 7.2

--- 1.4/scripts/start.sh
+++ 1.4/scripts/start.sh
@@ -3,16 +3,15 @@
 rsync -rc /tmp/humhub/* /usr/share/nginx/html
 
 # Display PHP error's or not
-if [[ "$ERRORS" != "1" ]] ; then
-  sed -i -e "s/error_reporting =.*=/error_reporting = E_ALL/g" /etc/php/7.0/fpm/php.ini
-  sed -i -e "s/display_errors =.*/display_errors = On/g" /etc/php/7.0/fpm/php.ini
+if [[ "$ERRORS" != "1" ]]; then
+    sed -i -e "s/error_reporting =.*=/error_reporting = E_ALL/g" /etc/php/7.2/fpm/php.ini
+    sed -i -e "s/display_errors =.*/display_errors = On/g" /etc/php/7.2/fpm/php.ini
 fi
 
 # Tweak nginx to match the workers to cpu's
 
-procs=$(cat /proc/cpuinfo |grep processor | wc -l)
+procs=$(cat /proc/cpuinfo | grep processor | wc -l)
 sed -i -e "s/worker_processes 5/worker_processes $procs/" /etc/nginx/nginx.conf
-
 
 mkdir -p /run/php/
 
