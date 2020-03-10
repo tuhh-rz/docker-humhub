3a9613d0d3673226540343a5f3637e23e8bcfb6d
Andreas Böttger <andreas.boettger@tuhh.de>
2020-03-10 11:02:30 +0100

    add 1.4

---
+++ 1.4/scripts/start.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+rsync -rc /tmp/humhub/* /usr/share/nginx/html
+
+# Display PHP error's or not
+if [[ "$ERRORS" != "1" ]] ; then
+  sed -i -e "s/error_reporting =.*=/error_reporting = E_ALL/g" /etc/php/7.0/fpm/php.ini
+  sed -i -e "s/display_errors =.*/display_errors = On/g" /etc/php/7.0/fpm/php.ini
+fi
+
+# Tweak nginx to match the workers to cpu's
+
+procs=$(cat /proc/cpuinfo |grep processor | wc -l)
+sed -i -e "s/worker_processes 5/worker_processes $procs/" /etc/nginx/nginx.conf
+
+
+mkdir -p /run/php/
+
+rm -rf /usr/share/nginx/html/protected/runtime/cache
+
+# http://docs.humhub.org/admin-updating.html
+su -s /bin/sh -c 'yes | php /usr/share/nginx/html/protected/yii migrate/up --includeModuleMigrations=1' www-data
+su -s /bin/sh -c 'php /usr/share/nginx/html/protected/yii module/update-all' www-data
+
+find /usr/share/nginx/html/ ! -user www-data -exec chown www-data: {} \;
+
+# Start supervisord and services
+exec /usr/bin/supervisord -n -c /etc/supervisord.conf
