7950dac73ebf175556eaddea739f9b7519542474
Andreas Böttger <andreas.boettger@tuhh.de>
2020-09-22 17:28:48 +0200

    optimize older versions

--- 1.3/scripts/start.sh
+++ 1.3/scripts/start.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-rsync -au /tmp/humhub/ /usr/share/nginx/html
+rsync -au /opt/humhub/ /usr/share/nginx/html
 
 # Display PHP error's or not
-if [[ "$ERRORS" != "1" ]] ; then
-  sed -i -e "s/error_reporting =.*=/error_reporting = E_ALL/g" /etc/php/7.0/fpm/php.ini
-  sed -i -e "s/display_errors =.*/display_errors = On/g" /etc/php/7.0/fpm/php.ini
+if [[ "$ERRORS" != "1" ]]; then
+    sed -i -e "s/error_reporting =.*=/error_reporting = E_ALL/g" /etc/php/7.2/fpm/php.ini
+    sed -i -e "s/display_errors =.*/display_errors = On/g" /etc/php/7.2/fpm/php.ini
 fi
 
 # Tweak nginx to match the workers to cpu's
-
-procs=$(cat /proc/cpuinfo |grep processor | wc -l)
+procs=$(grep -c processor /proc/cpuinfo)
 sed -i -e "s/worker_processes 5/worker_processes $procs/" /etc/nginx/nginx.conf
-
 
 mkdir -p /run/php/
 
@@ -20,9 +18,7 @@ rm -rf /usr/share/nginx/html/protected/runtime/cache
 
 # http://docs.humhub.org/admin-updating.html
 su -s /bin/sh -c 'yes | php /usr/share/nginx/html/protected/yii migrate/up --includeModuleMigrations=1' www-data
-su -s /bin/sh -c 'php /usr/share/nginx/html/protected/yii module/update-all' www-data
-
-find /usr/share/nginx/html/ ! -user www-data -exec chown www-data: {} +
+# su -s /bin/sh -c 'php /usr/share/nginx/html/protected/yii module/update-all' www-data
 
 # Start supervisord and services
 exec /usr/bin/supervisord -n -c /etc/supervisord.conf
